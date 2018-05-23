e2f1b9f7a26e7351257a4042b3069cbd07f8165d
Kevin Jolley <serrowpls@gmail.com>
2018-05-23 18:00:28 -0500

    update

--- src/App.js
+++ src/App.js
@@ -28,10 +28,10 @@ class App extends Component {
           <Body />
         </div>
         <div>
+          {/* <Filler />
           <Filler />
           <Filler />
-          <Filler />
-          <Filler />
+          <Filler /> */}
         </div>
       </div>
     );
--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -7,12 +7,8 @@ class Body extends Component {
   render() {
     return (
       <div>
-        <h1 className="bodyTitle">Check me out on Social Media!</h1>
-        <SocialIcon url="https://twitter.com/Serrowxd" />
-        <SocialIcon url="https://www.linkedin.com/in/kevin-jolley-26b600141/" />
-        <SocialIcon url="https://github.com/Serrowxd" />
+        <p> owo </p>
       </div>
-      // Add Social Media links here - Twitter, LinkedIn, Github, etc..
     );
   }
 }
