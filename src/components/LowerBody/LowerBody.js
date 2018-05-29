962de67cf346d67b04135b767276035dd699f969
Kevin Jolley <serrowpls@gmail.com>
2018-05-29 16:41:46 -0500

    update

--- src/components/LowerBody/LowerBody.js
+++ src/components/LowerBody/LowerBody.js
@@ -11,9 +11,9 @@ class LowerBody extends Component {
         <Container className="lowerC">
           <Row>
             <Col sm={{ size: 3, offset: 0 }} className="lowerCol">
-              <img class="img" src={img} alt="yeet" />
+              <img class="img" src={img} alt="shaprint" />
             </Col>
-            <Col sm={{ size: 8, offset: 1 }} className="lowerCol lowerR">
+            <Col sm={{ size: 9, offset: 0 }} className="lowerCol lowerR">
               <h1 className="lowerTxt">
                 <span> - Kevin Jolley </span>
               </h1>
@@ -25,25 +25,37 @@ class LowerBody extends Component {
                 screen. I always go at 100%, anything less is a waste of energy.
               </h4>
 
-              <h4>
+              <h4 className="lowhh">
                 My achievements in the technology industry are numerable - from
                 gaming to the real world;
               </h4>
               <ul>
-                <li> Top 10 Gross Sales in Texas | T-Mobile - 2014 </li>
-                <li> Tournament-level Elementalist | Guild Wars 2 - 2015 </li>
-                <li> Top 50 World Feral Druid | World of Warcraft - 2017 </li>
-                <li>
+                <li className="lowList">
+                  {' '}
+                  Top 10 Gross Sales in Texas | T-Mobile - 2014{' '}
+                </li>
+                <li className="lowList">
+                  {' '}
+                  Tournament-level Elementalist | Guild Wars 2 - 2015{' '}
+                </li>
+                <li className="lowList">
+                  {' '}
+                  Top 50 World Feral Druid | World of Warcraft - 2017{' '}
+                </li>
+                <li className="lowList">
                   Master of Techology, and all things that may not work |
                   Grandmother - 1998
                 </li>
-                <li> "Ok, but how did you do that?" | Mother - 2002 </li>
+                <li className="lowList">
+                  {' '}
+                  "Ok, but how did you do that?" | Mother - 2002{' '}
+                </li>
               </ul>
               <h4>
                 Though I only started my quest into the world of code, I hit the
                 ground running and haven't let up since.
               </h4>
-              <h4>
+              <h4 className="lowQuote">
                 <span>
                   “The clock is running. Make the most of today. Time waits for
                   no man. Yesterday is history. Tomorrow is a mystery. Today is
